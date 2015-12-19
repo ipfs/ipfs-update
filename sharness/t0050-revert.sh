0c83892d4e6a6dea0de24148f1e4892cc49219b4
Christian Couder <chriscool@tuxfamily.org>
2015-12-19 11:53:39 +0100

    sharness: add t0050-revert.sh

---
+++ sharness/t0050-revert.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+test_description="ipfs-update revert"
+
+. lib/test-lib.sh
+
+GUEST_IPFS_UPDATE="sharness/bin/ipfs-update"
+
+test_expect_success "start a docker container" '
+	DOCID=$(start_docker)
+'
+
+test_install_version "v0.3.9"
+test_install_version "v0.3.10"
+
+
+test_expect_success "'ipfs-update revert' works" '
+	exec_docker "$DOCID" "$GUEST_IPFS_UPDATE --verbose revert" >actual 2>&1 ||
+	test_fsh cat actual
+'
+
+test_expect_success "'ipfs-update revert' output looks good" '
+	grep "reverting to" actual &&
+	grep "revert complete." actual ||
+	test_fsh cat actual
+'
+
+test_expect_success "'ipfs-update version' works" '
+	exec_docker "$DOCID" "$GUEST_IPFS_UPDATE version" >actual
+'
+
+test_expect_success "'ipfs-update version' output looks good" '
+	echo "v0.3.9" >expected &&
+	test_cmp expected actual
+'
+
+test_expect_success "stop a docker container" '
+	stop_docker "$DOCID"
+'
+
+test_done
