cb879d9a3e0afc8e5cfa1a27114d70c266b2e82b
Christian Couder <chriscool@tuxfamily.org>
2015-12-19 11:36:27 +0100

    test-lib: add test_install_version()

--- sharness/lib/test-lib.sh
+++ sharness/lib/test-lib.sh
@@ -48,3 +48,27 @@ test_fsh() {
     echo ""
     false
 }
+
+test_install_version() {
+	VERSION="$1"
+
+	test_expect_success "'ipfs-update install' works for $VERSION" '
+		exec_docker "$DOCID" "$GUEST_IPFS_UPDATE --verbose install $VERSION" >actual 2>&1 ||
+		test_fsh cat actual
+	'
+
+	test_expect_success "'ipfs-update install' output looks good" '
+		grep "fetching ipfs version $VERSION" actual &&
+		grep "installation complete." actual ||
+		test_fsh cat actual
+	'
+
+	test_expect_success "'ipfs-update version' works for $VERSION" '
+		exec_docker "$DOCID" "$GUEST_IPFS_UPDATE version" >actual
+	'
+
+	test_expect_success "'ipfs-update version' output looks good" '
+		echo "$VERSION" >expected &&
+		test_cmp expected actual
+	'
+}
