d24bd364a28e611e585775ecd2d93f8b9713e60d
Christian Couder <chriscool@tuxfamily.org>
2015-12-21 18:55:54 +0100

    t0040: use ipfs init

--- sharness/t0040-install-many.sh
+++ sharness/t0040-install-many.sh
@@ -13,6 +13,19 @@ test_expect_success "start a docker container" '
 test_install_version "v0.3.7"
 test_install_version "v0.3.10"
 test_install_version "v0.4.0-dev"
+test_install_version "v0.3.8"
+
+test_expect_success "'ipfs init' succeeds" '
+	exec_docker "$DOCID" "IPFS_PATH=/root/.ipfs BITS=2048 ipfs init" >actual 2>&1 ||
+	test_fsh cat actual
+'
+
+test_expect_success ".ipfs/ has been created" '
+	exec_docker "$DOCID" "test -d  /root/.ipfs && test -f /root/.ipfs/config"
+	exec_docker "$DOCID" "test -d  /root/.ipfs/datastore && test -d /root/.ipfs/blocks"
+'
+
+test_install_version "v0.3.10"
 
 test_expect_success "stop a docker container" '
 	stop_docker "$DOCID"
