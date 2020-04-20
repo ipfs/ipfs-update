17bc1e512785516b700a15958835f4a1eeb0071b
Hector Sanjuan <code@hector.link>
2020-04-20 12:10:45 +0200

    Only allow downgrades with --downgrade flag

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ipfs/ipfs-update
 go 1.12
 
 require (
+	github.com/blang/semver v3.5.1+incompatible
 	github.com/coreos/go-semver v0.3.0
 	github.com/ipfs/go-ipfs-api v0.0.2
 	github.com/urfave/cli v1.22.1
--- main.go
+++ main.go
@@ -120,6 +120,10 @@ var cmdInstall = cli.Command{
 			Name:  "no-check",
 			Usage: "Skip pre-install tests.",
 		},
+		cli.BoolFlag{
+			Name:  "downgrade",
+			Usage: "Allow downgrading (and reverting any migrations).",
+		},
 	},
 	Action: func(c *cli.Context) error {
 		vers := c.Args().First()
@@ -139,7 +143,12 @@ var cmdInstall = cli.Command{
 			vers = "v" + vers
 		}
 
-		i, err := lib.NewInstall(util.IpfsVersionPath, vers, c.Bool("no-check"))
+		i, err := lib.NewInstall(
+			util.IpfsVersionPath,
+			vers,
+			c.Bool("no-check"),
+			c.Bool("downgrade"),
+		)
 		if err != nil {
 			return err
 		}
@@ -193,10 +202,16 @@ var cmdRevert = cli.Command{
 	Name:      "revert",
 	Usage:     "Revert to previously installed version of ipfs.",
 	ArgsUsage: " ",
-	Description: `'revert' will check if a previous update left a stashed
+	Description: `
+   'revert' will check if a previous update left a stashed
    binary and overwrite the current ipfs binary with it.
+
+   Using 'revert' will not run any datastore migrations. For that, use
+   'ipfs-update install --downgrade <prev-version>'.
+
    If multiple previous versions exist, you will be prompted to select the
-   desired binary.`,
+   desired binary.
+`,
 	Action: func(c *cli.Context) error {
 		oldbinpath, err := lib.SelectRevertBin()
 		if err != nil {
