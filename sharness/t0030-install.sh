8472f132b57b820737b60025698fc63a3230e997
gammazero <gammazero@users.noreply.github.com>
2021-03-31 12:34:09 -0700

    Update to use latest migrations lib

--- sharness/t0030-install.sh
+++ sharness/t0030-install.sh
@@ -7,7 +7,7 @@ test_description="ipfs-update install"
 GUEST_IPFS_UPDATE="sharness/bin/ipfs-update"
 
 # TODO: remove this when migrations are availabe on the distributions site
-IPFS_DIST_PATH="/ipfs/Qmaubnx6vDUEA2arLzPWxqY2brx2c1CUKsrgQHSwBXDZ5E"
+IPFS_DIST_PATH="/ipfs/QmVxxcTSuryJYdQJGcS8SyhzN7NBNLTqVPAxpu6gp2ZcrR"
 
 test_expect_success "start a docker container" '
 	DOCID=$(start_docker)
