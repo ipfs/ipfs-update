396207f50c7c01ae2b1119b39bb9ac5d1e0040ac
Gus Eggert <gus@gus.dev>
2022-01-06 15:21:12 -0500

    fix: allow repo migrations to v0.12

--- sharness/t0040-install-many.sh
+++ sharness/t0040-install-many.sh
@@ -12,9 +12,14 @@ test_expect_success "start a docker container" '
 
 test_install_version "v0.3.7"
 test_install_version "v0.3.10"
-test_install_version "v0.3.8"
-test_install_version "v0.4.0"
 
+# ensure downgrade works
+test_install_version "v0.3.7"
+
+# test upgrading repos across the v0.12 boundary
+test_install_version "v0.11.0"
+
+# init the repo so that migrations are run
 test_expect_success "'ipfs init' succeeds" '
 	exec_docker "$DOCID" "IPFS_PATH=/root/.ipfs BITS=2048 ipfs init" >actual 2>&1 ||
 	test_fsh cat actual
@@ -25,7 +30,8 @@ test_expect_success ".ipfs/ has been created" '
 	exec_docker "$DOCID" "test -d  /root/.ipfs/datastore && test -d /root/.ipfs/blocks"
 '
 
-test_install_version "v0.3.10"
+latest_version=$(curl -s https://dist.ipfs.io/go-ipfs/versions | tail -n 1)
+test_install_version "$latest_version"
 
 test_expect_success "stop a docker container" '
 	stop_docker "$DOCID"
